a7ce13e35f87c75c6a72fa73e24483cb9ccaff7a
nepersica <nepersica@gmail.com>
2025-12-18 16:07:07 +0900

    Update index.html

--- static/js/index.js
+++ static/js/index.js
@@ -29,17 +29,31 @@ $(document).ready(function() {
 
     });
 
+    // var options = {
+		// 	slidesToScroll: 1,
+		// 	slidesToShow: 3,
+		// 	loop: true,
+		// 	infinite: true,
+		// 	autoplay: false,
+		// 	autoplaySpeed: 3000,
+    // }
+
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      navigation: true,
+      pagination: false,
+      loop: true,
+      infinite: true,
+      autoplay: false,
     }
+
+    // bulmaCarousel.attach('.carousel', options);
+
 
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
+    
 
     // Loop on each carousel initialized
     for(var i = 0; i < carousels.length; i++) {
